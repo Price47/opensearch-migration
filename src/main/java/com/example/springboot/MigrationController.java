93531fb583fbcc64138759728d5cfc307c616ca2
Richard Stoeffel <richardstoeffel47@gmail.com>
2023-04-10 14:06:42 -0400

    NF: Initial demo work

---
+++ src/main/java/com/example/springboot/MigrationController.java
@@ -0,0 +1,25 @@
+package com.example.springboot;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.senacor.elasticsearch.evolution.core.ElasticsearchEvolution;
+
+@RestController
+public class MigrationController {
+
+	@GetMapping("/migrate")
+	public String index() {
+		// first create a Elastic RestClient
+		RestClient restclient = RestClient.builder(HttpHost.create("http://localhost:9200")).build();
+		// then create a ElasticsearchEvolution configuration and create a instance of ElasticsearchEvolution with that configuration
+		ElasticsearchEvolution elasticsearchEvolution = ElasticsearchEvolution.configure().load(restclient);
+		// execute the migration
+		int migrationsRun = elasticsearchEvolution.migrate();
+		
+		return String.format("%d Migrations Run", migrationsRun);
+	}
+
+}
